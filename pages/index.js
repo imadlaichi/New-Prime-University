0c2f8d957880d00d339bb3be940bf2b8a776de73
Imad Laichi <laichiimad141@gmail.com>
2024-01-21 11:02:27 +0100

    Update 1

--- pages/index.js
+++ pages/index.js
@@ -46,7 +46,7 @@ const Home = () => {
        <div 
        className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full 
        absolute mix-blend-color-dodge translate-z-0">
-
+        
        </div>
        {/* particles */} 
        <ParticlesContainer/>
