59303ac930a1141f3308dfcbd29a03640da861e0
imadlaichi <141133660+imadlaichi@users.noreply.github.com>
2024-01-23 17:02:34 +0100

    Update ServiceSlider2.js

--- components/ServiceSlider2.js
+++ components/ServiceSlider2.js
@@ -9,55 +9,55 @@ import {
 } from "react-icons/rx";
 
 const serviceData = [
-  {
+   {
     icon: <RxDiscordLogo/>,
-    title: 'Usuario 1',
-    description: '(⭐⭐⭐⭐⭐ )  El server esta bien pero le faltan cosas.... ( Poner lo que querais ) ',
+    title: 'Sara',
+    description: '⭐⭐⭐⭐⭐/ Increíble todo el contenido de valor que hay en esta comunidad. Súper recomendable!!!',
   },
   {
     icon: <RxDiscordLogo/>,
-    title: 'Usuario 2',
-    description: '(⭐⭐⭐⭐⭐ )  El server esta bien pero le faltan cosas.... ( Poner lo que querais )',
+    title: 'Samuel Diaz',
+    description: '⭐⭐⭐⭐⭐/Esta muy bueno el server muy completo la verdad tienen cursos valiosos ',
   },
   {
     icon: <RxDiscordLogo/>,
-    title: 'Usuario 3',
-    description: '(⭐⭐⭐⭐⭐ )  El server esta bien pero le faltan cosas.... ( Poner lo que querais )',
+    title: 'Joaquín Hernández',
+    description: '⭐⭐⭐⭐⭐/ Son increibles, la mejor comunidad!',
   },
   {
     icon: <RxDiscordLogo/>,
-    title: 'Usuario 4',
-    description: '(⭐⭐⭐⭐⭐ )  El server esta bien pero le faltan cosas.... ( Poner lo que querais )',
+    title: 'Manuel.Torres_',
+    description: '⭐⭐⭐⭐⭐/Que gran server, fue lo que esperaba',
   },
   {
     icon: <RxDiscordLogo/>,
-    title: 'Usuario 5',
-    description: '(⭐⭐⭐⭐⭐ )  El server esta bien pero le faltan cosas.... ( Poner lo que querais )',
+    title: 'NICOLAS serrano PALOMINO',
+    description: '⭐⭐⭐⭐⭐/ Uno de los mejores servidores de Discord, dan mucha información gratis y de buena calidad, poca gente así, lo recomiendo',
   },
   {
     icon: <RxDiscordLogo/>,
-    title: 'Usuario 6',
-    description: '(⭐⭐⭐⭐⭐ )  El server esta bien pero le faltan cosas.... ( Poner lo que querais )',
+    title: 'Luis Lara',
+    description: '⭐⭐⭐⭐⭐/ Es el mejor grupo para poder crecer gratuitamente ! Las 5 estrellas se las lleva mas que merecidas por todo el contenido gratuito que tiene !!',
   },
   {
     icon: <RxDiscordLogo/>,
-    title: 'Usuario 7',
-    description: '(⭐⭐⭐⭐⭐ )  El server esta bien pero le faltan cosas.... ( Poner lo que querais )',
+    title: 'Imad Laichi',
+    description: '⭐⭐⭐⭐⭐/ Una experiencia increíble. Este servidor de emprendimiento es el mejor que he encontrado. Ofrece una variedad de contenido para aprender y crecer, y la comunidad es positiva y llena de personas de alto valor. Muy recomendado.🎓',
   },
   {
     icon: <RxDiscordLogo/>,
     title: 'Usuario 8',
-    description: '(⭐⭐⭐⭐⭐ )  El server esta bien pero le faltan cosas.... ( Poner lo que querais )',
+    description: '⭐⭐⭐⭐⭐/ El server esta bien pero le faltan cosas.... ( Poner lo que querais )',
   },
   {
     icon: <RxDiscordLogo/>,
     title: 'Usuario 9',
-    description: '(⭐⭐⭐⭐⭐ )  El server esta bien pero le faltan cosas.... ( Poner lo que querais )',
+    description: '⭐⭐⭐⭐⭐/ El server esta bien pero le faltan cosas.... ( Poner lo que querais )',
   },
   {
     icon: <RxDiscordLogo/>,
     title: 'Usuario 10',
-    description: '(⭐⭐⭐⭐⭐ )  El server esta bien pero le faltan cosas.... ( Poner lo que querais )',
+    description: '⭐⭐⭐⭐⭐/ El server esta bien pero le faltan cosas.... ( Poner lo que querais )',
   },
   
   
