453e86bcb09aa630888890e9f28789c624778dc9
imadlaichi <141133660+imadlaichi@users.noreply.github.com>
2024-02-01 18:38:38 +0100

    Update index.js

--- pages/about/index.js
+++ pages/about/index.js
@@ -277,7 +277,7 @@ const About = () => {
             <div className="reative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 
             after:right-0 ">
               <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
-                <CountUp start={0} end={180} duration={5}/> +
+                <CountUp start={0} end={215} duration={5}/> +
                 </div>
               <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">Miembros</div>
             </div>
