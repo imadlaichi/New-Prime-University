f954a0e8058d6c3ba4a6e7461dfdd67a0e83af59
imadlaichi <141133660+imadlaichi@users.noreply.github.com>
2024-02-06 18:02:33 +0100

    Update Socials.js

--- components/Socials.js
+++ components/Socials.js
@@ -25,6 +25,12 @@ const Socials = () => {
     <Link href={"https://es.trustpilot.com/review/new-prime-university.netlify.app"} className="hover:text-secondary transition-all duration-300 text-2xl">
       <RiStarLine/>
     </Link>
+    <a href="agency" className="inline-block relative">
+  <button className="group relative bg-gradient-to-b from-[#ffffff] to-[#000000] h-max rounded-lg py-1 px-3 text-white font-semibold shadow-md transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-opacity-75">
+    <span className="absolute -inset-0.5 bg-gradient-to-b from-[#ffffff] to-[#1e1e1e] rounded-lg blur opacity-0 group-hover:opacity-100"></span>
+    <span className="relative z-10">Servicios</span>
+  </button>
+</a>
   </div>;
 };
 
