da5087d06c16190fd384777037b2fcc93455e992
imadlaichi <141133660+imadlaichi@users.noreply.github.com>
2024-02-14 17:19:00 +0100

    Update ProjectsBtn.js

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -9,7 +9,7 @@ import { HiAcademicCap} from "react-icons/hi2";
 
 const ProjectsBtn = () => {
   return <div className="mx-auto xl:mx-0 z-10">
-     <Link href={"https://discord.gg/xM576kRdWH"} className="relative w-[185px] h-[185px] flex justify-center items-center bg-circleS  tar bg-cover bg-center bg-no-repeat group">
+     <Link href={"https://discord.gg/ckJgsmeEw4"} className="relative w-[185px] h-[185px] flex justify-center items-center bg-circleS  tar bg-cover bg-center bg-no-repeat group">
       < Image src={"/join-text.png"} width={141} height={148} alt="" 
       className="animate-spin-slow w-full h-full max-w-[141px] max-h-[148px]"/>
         <HiAcademicCap className="absolute text-5xl group-hover:text-6xl transition-all duration-300 "/>
