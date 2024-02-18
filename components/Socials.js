2b73b7d58c9b21651a90981da1134cea7c2d9ba4
imadlaichi <141133660+imadlaichi@users.noreply.github.com>
2024-02-19 00:32:14 +0100

    Update Socials.js

--- components/Socials.js
+++ components/Socials.js
@@ -32,8 +32,8 @@ const Socials = () => {
   </button>
 </a>
     <a href="https://primebrand.store" className="inline-block relative">
-  <button className="group relative bg-gradient-to-b from-[#FFD700] to-[#000000] h-max rounded-lg py-1 px-3 text-white font-semibold shadow-md transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-opacity-75">
-    <span className="absolute -inset-0.5 bg-gradient-to-b from-[#ffffff] to-[#1e1e1e] rounded-lg blur opacity-0 group-hover:opacity-100"></span>
+  <button className="group relative bg-gradient-to-b from-[#FFD700] to-[#ffffff] h-max rounded-lg py-1 px-3 text-white font-semibold shadow-md transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-opacity-75">
+    <span className="absolute -inset-0.5 bg-gradient-to-b from-[#FFD700] to-[#1e1e1e] rounded-lg blur opacity-0 group-hover:opacity-100"></span>
     <span className="relative z-10">Prime Brand</span>
   </button>
 </a>
