c51f520bea724f3abe3a4adb0e5a8c619d2a1944
imadlaichi <141133660+imadlaichi@users.noreply.github.com>
2024-02-20 22:24:12 +0100

    Update index.js

--- pages/agency/index.js
+++ pages/agency/index.js
@@ -1,5 +1,3 @@
-import AgencySlider from "../../components/AgencySlider";
-import Bulb from "../../components/Bulb";
 import Circles from "../../components/Circles";
 import { motion } from "framer-motion";
 import { fadeIn } from "../../variants";
