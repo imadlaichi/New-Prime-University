2b7c7cc6256e4ec24dad52feb998fedac46149ff
imadlaichi <141133660+imadlaichi@users.noreply.github.com>
2024-02-20 22:19:34 +0100

    Update index.js

--- pages/agency/index.js
+++ pages/agency/index.js
@@ -1,32 +1,47 @@
-
-// components
-import ServiceSlider2 from "../../components/ServiceSlider2";
+import AgencySlider from "../../components/AgencySlider";
 import Bulb from "../../components/Bulb";
 import Circles from "../../components/Circles";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
+import HoverDevCards from "../../components/HoverDevCards";
+import Avatar from "../../components/Avatar";
+const Agency = () => {
+  return (
+    <div className="h-full bg-primary/30 py-36 flex items-center overflow-hidden">
+      <Circles />
+      <motion.div
+        variants={fadeIn("right", 0.2)}
+        initial="hidden"
+        animate="show"
+        exit="hidden"
+        className="hidden xl:flex absolute bottom-0 -left-[370px] "
+      >
+        <Avatar />
+      </motion.div>
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row gap-x-8">
+          {/* Texto */}
+          <div className="text-center lg:w-[50%] lg:text-left mb-4 lg:mb-0 lg:order-1">
+            <motion.div variants={fadeIn("up", 0.2)} initial="hidden" animate="show" exit="hidden">
+              <h2 className="h2 lg:mt-8">Servicios Prime<span className="text-accent"></span></h2>
+              <p className="mb-4 max-w-[400px] mx-auto lg:mx-0">Haz que tu proyecto alcance su prime con nuestros servicios de calidad.</p>
+         
+         
+            </motion.div>
+          </div>
 
-// framer motion
-import {motion} from "framer-motion"
-import {fadeIn} from "../../variants"
+          
 
-const Testimonial = () => {
-  return <div className="h-full bg-primary/30 py-36 flex items-center">
-    <Circles/>
-    <div className="container mx-auto">
-      <div className="flex flex-col xl:fex-row gap-x-8">
-        {/* text */}
-        <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
-        <motion.h2 variants={fadeIn("up",0.2)} initial="hidden" animate="show" exit="hidden" 
-        className="h2 xl:mt-8">Proximamente<span className="text-accent"></span></motion.h2>
-        <motion.p variants={fadeIn("up",0.4)} initial="hidden" animate="show" exit="hidden" 
-        className="mb-4 max-w-[400px] mx-auto lg:mx-0">Prime agency te ofrece los mejores servicios para sacar el prime de tu proyecto</motion.p>
         </div>
-        
-        
+        <motion.div variants={fadeIn("down", 0.4)} initial="hidden" animate="show" exit="hidden">
+          <HoverDevCards/>
+        </motion.div>
         
       </div>
+      
+      
     </div>
-    <Bulb/>
-  </div>;
+  );
 };
 
-export default Testimonial;
+export default Agency;
