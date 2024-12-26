95fe64c2d191d4dfca976cf9f33aca7c6f6008d1
Imad Laichi <141133660+imadlaichi@users.noreply.github.com>
2024-12-26 14:21:37 +0100

    Update ProjectsBtn.js

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -5,14 +5,14 @@ import Image from "next/image";
 import Link from "next/link";
 
 // icons 
-import { HiAcademicCap} from "react-icons/hi2";
+import { HiCodeBracket} from "react-icons/hi2";
 
 const ProjectsBtn = () => {
   return <div className="mx-auto xl:mx-0 z-10">
-     <Link href={"https://discord.gg/ckJgsmeEw4"} className="relative w-[185px] h-[185px] flex justify-center items-center bg-circleS  tar bg-cover bg-center bg-no-repeat group">
-      < Image src={"/join-text.png"} width={141} height={148} alt="" 
+     <Link href={"https://discord.gg/yyMzyknWm3"} className="relative w-[185px] h-[185px] flex justify-center items-center bg-circleS  tar bg-cover bg-center bg-no-repeat group">
+      < Image src={"/text-for-button.png"} width={141} height={148} alt="" 
       className="animate-spin-slow w-full h-full max-w-[141px] max-h-[148px]"/>
-        <HiAcademicCap className="absolute text-5xl group-hover:text-6xl transition-all duration-300 "/>
+        <HiCodeBracket className="absolute text-5xl group-hover:text-6xl transition-all duration-300 "/>
      </Link>
   </div>;
 };
